82e90fe38c42f5ffc3051fee8a5ddf0fc7012135
prashanthmadi <prashanthrmadi@gmail.com>
2017-06-07 20:38:41 -0500

    calculating fibnonci in webapp

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from math import sqrt
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Fibnonci of 10 using <br> Normal Approach:"+fibnonci_normal(10)+"  <br> Easy Formula Approach"+fibnonci_easy(10))
+
+
+def fibnonci_normal(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fibnonci_normal(n-1)+fibnonci_normal(n-2)
+
+def fibnonci_easy(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
